94b0d000d5312832c26bdbccca747357ad942dfb
Giuseppe11S <senagiuseppe11@gmail.com>
2025-11-08 14:09:09 +0100

    Aggiungo props a CardViaggi e renderizzato nome viaggio, data inizio e fine del viaggio in Homepage

--- src/components/CardViaggi.jsx
+++ src/components/CardViaggi.jsx
@@ -1,1 +1,12 @@
 // props per HomePage.jsx
+
+export default function CardViaggi({id, destinazione, dataInizio, dataFine}) {
+
+  return (
+    <>
+      <h2>{destinazione}</h2>
+       <span>{dataInizio}</span>
+       <span>{dataFine}</span>
+    </>
+  )
+}
