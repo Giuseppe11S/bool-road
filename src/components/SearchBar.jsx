2bbc651d9120c3561143d5c20d7451872f226cca
Aleiaco02 <alessandroiacovelli83@gmail.com>
2025-11-08 13:49:36 +0100

    creato componente SearchBar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+import { viaggi } from "../data/data"
+import { useState } from "react"
+
+const SearchBar = () => {
+    return (
+        <div>SearchBar</div>
+    )
+}
+
+export default SearchBar
