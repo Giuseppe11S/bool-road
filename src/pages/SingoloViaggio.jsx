2f10df8bc0046fa9b554c766e565cc2cb3685300
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-11-08 11:54:02 +0100

    - aggiunto gli id al file data.js - aggiunta logica props singolo viaggio (useParams + find)

--- src/pages/SingoloViaggio.jsx
+++ src/pages/SingoloViaggio.jsx
@@ -1,12 +1,17 @@
+import { useParams } from "react-router-dom";
+import { viaggi } from "../data/data";
 
-
-import React from 'react'
+import React from "react";
 
 const SingoloViaggio = () => {
-  return (
-    <>
-    </>
-  )
-}
+  const { id } = useParams();
 
-export default SingoloViaggio
+  const viaggio = viaggi.find((viaggio) => viaggio.id === parseInt(id));
+
+  if (!viaggio) {
+    return <p>Viaggio non trovato</p>;
+  }
+  return viaggio;
+};
+
+export default SingoloViaggio;
