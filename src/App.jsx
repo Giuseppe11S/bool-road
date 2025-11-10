c6e55338447a38b1e16048ba240f49e9a7b4e97b
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-11-10 10:26:06 +0100

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,26 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // import pages
-import HomePage from './pages/HomePage' 
-import SingoloViaggio from './pages/SingoloViaggio'
+import HomePage from "./pages/HomePage";
+import SingoloViaggio from "./pages/SingoloViaggio";
 
 // Default Layout
-import DefaultLayout from './layouts/DefaultLayout'
+import DefaultLayout from "./layouts/DefaultLayout";
 
 function App() {
-
   return (
-
     <>
-     <BrowserRouter>
+      <BrowserRouter>
         <Routes>
-              <Route element={<DefaultLayout/>}>
-                <Route path='/'
-                element={<HomePage/>}></Route>
-                <Route path='/:id' 
-                element={<SingoloViaggio/>}></Route>
-              </Route>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/:id" element={<SingoloViaggio />}></Route>
+          </Route>
         </Routes>
-     </BrowserRouter>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
