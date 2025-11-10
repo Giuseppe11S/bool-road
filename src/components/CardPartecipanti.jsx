058cacc31b5e080ccea985f50128f572cf5ef37f
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-11-10 14:45:38 +0100

    css singolo viaggio e card partecipanti

--- src/components/CardPartecipanti.jsx
+++ src/components/CardPartecipanti.jsx
@@ -1,32 +1,38 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const CardPartecipanti = ({ partecipante, isOpen, onToggle }) => {
-
-    /***************
+  /***************
         RENDERING
     ***************/
-    return (
+  return (
+    <div className="partecipante-card">
+      {/* Header Accordion */}
+      <div className="partecipante-header" onClick={onToggle}>
+        <img src={partecipante.image} alt="" className="img-partecipante" />
+        <h3>
+          {" "}
+          {partecipante.nome} {partecipante.cognome}{" "}
+          <span className="open-closed">{isOpen ? " ︿" : " ﹀"}</span>{" "}
+        </h3>
+      </div>
 
-        <div className='partecipante-card'>
-
-            {/* Header Accordion */}
-            <div className="partecipante-header" onClick={onToggle}>
-                <img src={partecipante.image} alt="" />
-                <h3> {partecipante.nome} {partecipante.cognome} <span>{isOpen ? '−' : '+'}</span> </h3>
-            </div>
-
-            {/* Dettagli partecipanti (visibili solo se la card è aperta) */}
-            {isOpen &&
-                <address className="partecipante-details">
-
-                    <p><strong>Codice Fiscale:</strong> {partecipante.cf}</p>
-                    <p><strong>Telefono:</strong> {partecipante.telefono}</p>
-                    <p><strong>Email:</strong> {partecipante.email}</p>
-                </address>
-            }
-        </div>
-    )
-}
+      {/* Dettagli partecipanti (visibili solo se la card è aperta) */}
+      {isOpen && (
+        <address className="partecipante-details">
+          <hr />
+          <p>
+            <strong>Codice Fiscale:</strong> {partecipante.cf}
+          </p>
+          <p>
+            <strong>Telefono:</strong> {partecipante.telefono}
+          </p>
+          <p>
+            <strong>Email:</strong> {partecipante.email}
+          </p>
+        </address>
+      )}
+    </div>
+  );
+};
 
 export default CardPartecipanti;
-
